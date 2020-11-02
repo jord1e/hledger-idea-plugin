591a861f5f4bfb1ccb49f62d8184fbac35000c51
Jordie <git@jordie24.nl>
2020-11-02 19:39:07 +0100

    removed old files

--- src/main/kotlin/nl/jord1e/hledger/idea/journal/JournalLanguage.kt
+++
@@ -1,6 +0,0 @@
-package nl.jord1e.hledger.idea.journal
-
-import com.intellij.lang.Language
-
-object JournalLanguage : Language("JOURNAL") {
-}
--- src/main/kotlin/nl/jord1e/hledger/idea/journal/psi/JournalElementType.kt
+++
@@ -1,7 +0,0 @@
-package nl.jord1e.hledger.idea.journal.psi
-
-import com.intellij.psi.tree.IElementType
-import nl.jord1e.hledger.idea.journal.JournalLanguage
-
-class JournalElementType(debugName: String) : IElementType(debugName, JournalLanguage) {
-}
